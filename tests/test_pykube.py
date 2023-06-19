87ec976be929ef028731b57a86054158e00e7701
Deepika <dejain4@gmail.com>
2023-06-19 07:56:19 +0530

    :sparkles: initial commit

---
+++ tests/test_pykube.py
@@ -0,0 +1,11 @@
+import pytest
+
+from pykube import app
+
+
+def test_hello():
+    client = app.test_client()
+    response = client.get('/')
+    assert response.status_code == 200
+    assert response.data == b'Hello, World!'
+
